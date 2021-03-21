f0f1ba64d51556996cf3a42ad8895a600798e826
pgm-arthtemm <arthur.temmerman@student.arteveldehs.be>
2021-03-21 12:53:54 +0100

    added filter

--- src/middleware/todofilter.js
+++ src/middleware/todofilter.js
@@ -1,6 +1,6 @@
 /**
  * Creating Todo Filter middleware
- */
+ */ 
 
 export default (req, resp, next) => {
   if (!req.body.todo) {
@@ -9,7 +9,8 @@ export default (req, resp, next) => {
   }
 
   const { description } = req.body.todo;
-  if (description.includes('damn')) {
+  let isInclude = (process.env.BADWORDS_FILEPATH).some(word => description.includes(word));
+  if (!isInclude) {
     resp.status(406).json({ error: 'This language is not acceptable!' });
   } else {
     next();
